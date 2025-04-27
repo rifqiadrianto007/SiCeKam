58e37c29f4413b9eb702118f90c1185514e2bdb5
rifqiadrianto007 <232410103042@mail.unej.ac.id>
2025-04-28 00:01:31 +0700

    sidebar component

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -30,53 +30,7 @@
 <body class="bg-gray-100">
     <div class="min-h-screen flex flex-col flex-auto flex-shrink-0 antialiased">
         <!-- Sidebar -->
-        <div class="fixed flex flex-col top-0 left-0 w-64 bg-indigo-500 h-full shadow-lg sidebar">
-            <div class="flex items-center justify-center h-14 border-b border-indigo-300">
-                <div class="text-white text-xl font-bold">SiCekam</div>
-            </div>
-            <div class="overflow-y-auto overflow-x-hidden flex-grow">
-                <ul class="flex flex-col space-y-1 py-4">
-                    <li>
-                        <a href="{{ route('admin') }}" class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-indigo-400 text-white-600 hover:text-white-800 border-l-4 border-transparent hover:border-blue-500 pr-6 {{ request()->routeIs('admin') ? 'active-nav border-blue-500 bg-indigo-400' : '' }}">
-                            <span class="inline-flex justify-center items-center ml-4">
-                                <i class="fas fa-tachometer-alt"></i>
-                            </span>
-                            <span class="ml-2 text-sm tracking-wide truncate text-white">Dashboard</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="{{ route('ayam') }}" class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-indigo-400 text-white-600 hover:text-white-800 border-l-4 border-transparent hover:border-blue-500 pr-6 {{ request()->routeIs('ayam*') ? 'active-nav border-blue-500 bg-indigo-400' : '' }}">
-                            <span class="inline-flex justify-center items-center ml-4">
-                                <i class="fas fa-warehouse"></i>
-                            </span>
-                            <span class="ml-2 text-sm tracking-wide truncate text-white">
-                                Blok Kandang
-                            </span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="{{ route('akun') }}" class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-indigo-400 text-white-600 hover:text-white-800 border-l-4 border-transparent hover:border-blue-500 pr-6 {{ request()->routeIs('akun*') ? 'active-nav border-blue-500 bg-indigo-400' : '' }}">
-                            <span class="inline-flex justify-center items-center ml-4">
-                                <i class="fas fa-users-cog"></i>
-                            </span>
-                            <span class="ml-2 text-sm tracking-wide truncate text-white">
-                                Manajemen Pengguna
-                            </span>
-                        </a>
-                    </li>
-                    <li class="mt-auto">
-                        <a href="{{ route('login') }}" class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-indigo-400 text-white-600 hover:text-white-800 border-l-4 border-transparent hover:border-blue-500 pr-6">
-                            <span class="inline-flex justify-center items-center ml-4">
-                                <i class="fas fa-sign-out-alt"></i>
-                            </span>
-                            <span class="ml-2 text-sm tracking-wide truncate text-white">
-                                Keluar
-                            </span>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </div>
+        <x-sidebar-admin />
 
         <div class="ml-64 p-6">
             <!-- Header -->
