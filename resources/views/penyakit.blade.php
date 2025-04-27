422df84e029aad11b9d6a45e5595e63cc6cadfcd
rifqiadrianto007 <232410103042@mail.unej.ac.id>
2025-04-28 00:12:17 +0700

    Button Blok Components

--- resources/views/penyakit.blade.php
+++ resources/views/penyakit.blade.php
@@ -77,23 +77,7 @@
     </div>
 
     {{-- Halaman Pemilihan Blok --}}
-    <div id="class-selection" class="h-full w-full flex flex-col justify-center items-center p-6 bg-gray-50">
-        <h1 class="text-2xl font-bold mb-6">Pilih Kelas Kandang</h1>
-        <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 gap-4 mb-6">
-            <!-- Tombol kelas A1 - A10 -->
-            <button onclick="selectClass('A1')" class="class-btn">A1</button>
-            <button onclick="selectClass('A2')" class="class-btn">A2</button>
-            <button onclick="selectClass('A3')" class="class-btn">A3</button>
-            <button onclick="selectClass('A4')" class="class-btn">A4</button>
-            <button onclick="selectClass('A5')" class="class-btn">A5</button>
-            <button onclick="selectClass('A6')" class="class-btn">A6</button>
-            <button onclick="selectClass('A7')" class="class-btn">A7</button>
-            <button onclick="selectClass('A8')" class="class-btn">A8</button>
-            <button onclick="selectClass('A9')" class="class-btn">A9</button>
-            <button onclick="selectClass('A10')" class="class-btn">A10</button>
-        </div>
-        <button id="lanjut-btn" class="bg-primary text-indigo-500 font-semibold py-3 px-6 rounded-lg hover:opacity-90 transition disabled:opacity-50" disabled>Lanjutkan</button>
-    </div>
+    <x-PilihBlok />
 
     {{-- Halaman Scan Penyakit --}}
     <div class="content-container flex-1 py-10 overflow-auto" id="deteksi-section">
@@ -150,6 +134,7 @@
                         </label>
                     </div>
 
+                    {{-- BAGIAN HASIL --}}
                     <div id="preview-container" class="hidden mt-6">
                         <h4 class="text-lg font-medium text-gray-800 mb-4">Pratinjau Foto</h4>
 
